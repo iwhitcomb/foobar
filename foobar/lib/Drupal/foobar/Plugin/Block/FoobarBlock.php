754e5ff81067b6e62b6f7b0c74e9191151fa8d63
Ian Whitcomb <ianw@getlevelten.com>
2013-07-05 16:21:51 -0500

    Added some additional foobar.info.yml settings and an example for creating blocks

---
+++ foobar/lib/Drupal/foobar/Plugin/Block/FoobarBlock.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\foobar\Plugin\Block\FoobarBlock.
+ */
+
+namespace Drupal\foobar\Plugin\Block;
+
+use Drupal\block\BlockBase;
+use Drupal\Component\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
+
+/**
+ * Provides a 'Foobar' block.
+ *
+ * @Plugin(
+ *   id = "foobar_foobar_block",
+ *   admin_label = @Translation("Foobar"),
+ *   module = "foobar"
+ * )
+ */
+class FoobarBlock extends BlockBase {
+
+  /**
+   * Overrides \Drupal\block\BlockBase::settings().
+   */
+  public function settings() {
+    return array(
+      'block_count' => 10,
+    );
+  }
+
+  /**
+   * Overrides \Drupal\block\BlockBase::access().
+   */
+  public function access() {
+    return user_access('access content');
+  }
+
+  /**
+   * Implements \Drupal\block\BlockBase::blockBuild().
+   */
+  protected function blockBuild() {
+    return array(
+      '#markup' => t('This is an example of how to create blocks for Drupal 8.'),
+    );
+  }
+
+}
